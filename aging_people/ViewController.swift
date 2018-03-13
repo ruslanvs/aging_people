d2b3e3cb866d9e2a6fc4f0056ada2842357cb317
ruslanvs <ruslanvs@yahoo.com>
2018-03-12 17:17:16 -0700

    Assignment complete

--- aging_people/ViewController.swift
+++ aging_people/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var items = ["Max", "Tom", "Sol", "John", "Betty", "Lana", "Don", "Ran", "Solo", "Mio", "Bella", "Donna"]
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell( withIdentifier: "MyCell", for: indexPath )
+        
+        cell.textLabel?.text = items[indexPath.row]
+        cell.detailTextLabel?.text = "\(arc4random_uniform(96)) years old"
+        return cell
+    }
+}
